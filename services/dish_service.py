73a600d756c57fc8c393cc632b20095d77c177a9
MINGJIA MAO <toddymao0301@gmail.com>
2025-07-02 17:57:58 -0700

    fix(module): add ingredients

--- services/dish_service.py
+++ services/dish_service.py
@@ -69,11 +69,15 @@ class DishService:
 
                 dish._ai_flavor_tags  = [tag for tag in formatted_tags
                                         if tag['category'] in ['basic_flavor', 'detail_flavor', 'texture']]
+                dish._ingredients_data = [tag for tag in formatted_tags
+                                        if tag['category'] in ['basic_flavor', 'detail_flavor', 'texture']]
                 dish._ai_unique_tags  = [tag['tag'] for tag in formatted_tags
                                          if tag['category'] == ['texture', 'combined_texture', 'cuisine']]
             else:
                 dish._ai_flavor_tags = []
                 dish._ai_unique_tags = []
+
+
 
             #logic for show if current user collected or recommended
             is_collected = False
